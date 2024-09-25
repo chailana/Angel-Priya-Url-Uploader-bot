8868271377cbaf73f8ad8a1e55086823ebd9026d
chailana <chailana35217793@gmail.com>
2024-09-25 17:21:31 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -175,7 +175,7 @@ async def youtube_dl_call_back(bot, update):
         return False
     if t_response:
         # logger.info(t_response)
-        os.remove(save_ytdl_json_path)
+        #os.remove(save_ytdl_json_path)
         end_one = datetime.now()
         time_taken_for_download = (end_one -start).seconds
         file_size = Config.TG_MAX_FILE_SIZE + 1
