c04518e83bcd9bd55931ef8f64845f2a0127e2c8
chailana <chailana35217793@gmail.com>
2024-09-25 15:33:19 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -100,7 +100,7 @@ async def youtube_dl_call_back(bot, update):
     await bot.edit_message_text(
         text=Translation.DOWNLOAD_START,
         chat_id=update.message.chat.id,
-        message_id=update.id
+        message_id=update.message.id
     )
     user = await bot.get_me()
     mention = user["mention"]
